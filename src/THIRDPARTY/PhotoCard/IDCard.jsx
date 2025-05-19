e043241aa0cfcbdd391dcab5272dcda7997a0bce
Anand Kumar Jaiswal <108394963+ANAND2023@users.noreply.github.com>
2025-05-19 11:14:12 +0530

    Update IDCard.jsx
    responsive mt

--- src/THIRDPARTY/PhotoCard/IDCard.jsx
+++ src/THIRDPARTY/PhotoCard/IDCard.jsx
@@ -155,6 +155,7 @@ if (!SchoolID) {
         <>
           <div
         className="py-[1px] fixed top-[70px] w-full  z-10"
+             className="py-[1px] fixed top-[95px] w-full  z-10 bg-white"
         // style={{ background: "#2fa7db" }}
       >
         <div className="flex justify-around max-w-md mx-auto gap-1">
